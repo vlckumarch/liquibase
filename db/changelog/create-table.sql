ad643041ecfb6c496c4cb63fa4f86bce07c47ac1
vlckumarch <52064244+vlckumarch@users.noreply.github.com>
2025-03-20 23:34:20 +0530

    Update create-table.sql

--- db/changelog/create-table.sql
+++ db/changelog/create-table.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 
 --changeset your-name:create_users_table
-CREATE TABLE users222 (
+CREATE TABLE users322 (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL
